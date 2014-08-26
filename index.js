3853d089b685407293b19c2b9d82690211485528
Mihai Gomoi <mihai.gomoi@softvision.ro>
2014-08-26 15:13:46 +0300

    Serve static html

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	var http = require('http'),
+		fs = require('fs');
+
+
+	fs.readFile('./index.html', function (err, html) {
+		if (err) {
+			throw err; 
+		}       
+		http.createServer(function(request, response) {  
+			response.writeHeader(200, {"Content-Type": "text/html"});  
+			response.write(html);  
+			response.end();  
+		}).listen(8000);
+	});
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
