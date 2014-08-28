05751bacd63a4de4e680d87ec0fe88f3a759e5d4
Mihai Gomoi <mihai.gomoi@softvision.ro>
2014-08-28 10:09:50 +0300

    Serve plain html

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var express = require('express')
 var app = express();
-var cool = require('cool-ascii-faces')
-
-app.use(express.logger());
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  es.sendfile(__dirname + '/index.html');
 })
 
 app.listen(app.get('port'), function() {
