a3993da4682a97fdcb0543bcc6c04f689cf3d98b
Mihai Gomoi <mihai.gomoi@softvision.ro>
2014-08-28 10:15:11 +0300

    Test express

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var cool = require('cool-ascii-faces');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/2048'))
 
-app.get('/', function(request, response) {
-  es.sendfile(__dirname + '/2048/index.html');
-})
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
