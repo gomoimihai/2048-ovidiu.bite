c90f36a09216af8ba65ab864edd7a5a9446234fe
Mihai Bogdan Gomoi <gomoimihai@gmail.com>
2015-05-21 11:55:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.post("/public", function(req, res, next) {
+  req.method = "GET";
+  next();
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
